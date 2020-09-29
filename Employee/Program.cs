5f225d1a742ea10f873624d5d0ee928bd2b180f0
azmon6 <kaloqn_bss@abv.bg>
2020-09-30 01:14:57 +0300

    Add files via upload

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -8,71 +8,68 @@ namespace Employee
 
     class Program
     {
-        static void Main(string[] args)
+
+        private static string[][] data;
+        private static Dictionary<string, int> workingTimeTogether ;
+
+        private static DateTime StringToDate(string temp)
+        {
+            if (temp.ToLower() == "null")
+                return DateTime.Now;
+
+            return DateTime.Parse(temp);
+        }
+
+        private static bool LoadData()
         {
             Console.WriteLine("Write absolute path to txt file.");
             string testFileName = Console.ReadLine();
-            if(testFileName == null || !File.Exists(testFileName))
+            if (testFileName == null || !File.Exists(testFileName))
             {
                 Console.WriteLine("File Not Found.");
-                return;
+                return false;
             }
-            string[][] data = File.ReadAllLines(testFileName).Select(x => x.Split(',')).ToArray();
-            Dictionary<string, int> workingTimeTogether = new Dictionary<string, int>();
+
+            workingTimeTogether = new Dictionary<string, int>();
+            data = File.ReadAllLines(testFileName).Select(x => x.Split(',').Select(z => z.Trim()).ToArray()).ToArray();
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            if (!LoadData())
+                return;
+
             for(int i = 0; i< data.GetLength(0);i++)
             {
                 for(int p = i+1; p < data.GetLength(0); p++)
                 {
                     if (data[i][0] == data[p][0] || data[i][1] != data[p][1])
                         continue;
-                    DateTime firstEmpStart = Convert.ToDateTime(data[i][2]);
-                    DateTime secondEmpStart = Convert.ToDateTime(data[p][2]);
-                    DateTime firstEmpEnd;
-                    DateTime secondEmpEnd;
-                    if (data[i][3] == " Null")
-                    {
-                        firstEmpEnd = DateTime.Now;
-                    }
-                    else
-                    {
-                        firstEmpEnd = Convert.ToDateTime(data[i][3]);
-                    }
 
-                    if(data[p][3] == " Null")
-                    {
-                        secondEmpEnd = DateTime.Now;
-                    }
-                    else
-                    {
-                        secondEmpEnd = Convert.ToDateTime(data[p][3]);
-                    }
+                    DateTime firstEmpStart = StringToDate(data[i][2]);
+                    DateTime secondEmpStart = StringToDate(data[p][2]);
+                    DateTime firstEmpEnd = StringToDate(data[i][3]);
+                    DateTime secondEmpEnd = StringToDate(data[p][3]);
 
-                    double timeWorkedTogether = (firstEmpStart - secondEmpStart).TotalDays;
-                    double temp = (firstEmpStart - secondEmpStart).TotalDays;
-                    double temp2 = (firstEmpEnd - secondEmpEnd).TotalDays;
-
-                    if (timeWorkedTogether <= 0)
+                    if (secondEmpStart > firstEmpEnd || firstEmpStart > secondEmpEnd)
                         continue;
 
-                    if ( temp > 0 )
-                    {
-                        timeWorkedTogether -= temp;
-                    }
+                    DateTime timeWorkedStart = (firstEmpStart > secondEmpStart ? firstEmpStart : secondEmpStart);
+                    DateTime timeWorkedEnd = (firstEmpEnd < secondEmpEnd ? firstEmpEnd : secondEmpEnd);
+                    double timeWorkedTogether = (timeWorkedEnd - timeWorkedStart).TotalDays;
 
-                    if(temp2 > 0 )
-                    {
-                        timeWorkedTogether -= temp2;
-                    }
-
-                    string empKey = data[i][0] + "," + data[p][0];
-                    if(!workingTimeTogether.ContainsKey(empKey))
-                    {
-                        workingTimeTogether.Add(empKey, Convert.ToInt32(timeWorkedTogether));
-                    }
+                    string empKey;
+                    if (int.Parse(data[i][0]) > int.Parse(data[p][0]))
+                        empKey = data[i][0] + "," + data[p][0];
                     else
-                    {
+                        empKey = data[p][0] + "," + data[i][0];
+
+                    if(!workingTimeTogether.ContainsKey(empKey))
+                        workingTimeTogether.Add(empKey, Convert.ToInt32(timeWorkedTogether));
+                    else
                         workingTimeTogether[empKey] += Convert.ToInt32(timeWorkedTogether);
-                    }
                 }
             }
 
@@ -83,8 +80,7 @@ namespace Employee
             }
 
             string keyOfMaxValue = workingTimeTogether.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
-            string[] id = keyOfMaxValue.Split(",");
-            Console.WriteLine("Employees worked together the most are: " + id[0] + " " + id[1]);
+            Console.WriteLine("Employees worked together the most are: " + keyOfMaxValue + " Days: " + workingTimeTogether[keyOfMaxValue]);
         }
     }
 }
